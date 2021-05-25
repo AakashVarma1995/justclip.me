919d413f534025a5aea19dc85f927676c9bfb5b9
Aakash Varma <me@aakashvarma.dev>
2021-05-25 14:53:02 +0530

    Added Git Repo Link

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -8,10 +8,21 @@ const useStyles = createUseStyles({
         zIndex:1000,
         display:'flex',
         alignItems:'center',
+        justifyContent:'space-between',
         boxShadow:'0px -12px 9px 10px grey'
     },
     logo:{
         marginLeft:'16px'
+    },
+    git:{
+        background:'#530BBD',
+        padding:'4px',
+        borderRadius:'4px',
+        marginRight:'16px',
+        transition:'.3s all',
+        '&:hover':{
+            background:'#F31489'
+        }
     }
   })
 
@@ -21,6 +32,9 @@ export default function Navbar(props) {
         <div className={classes.root}>
             <div className={classes.logo}>
                 <a href="/"><img src={'/assets/logo.svg'} style={{height:'32px'}} alt="JustClipMe Logo"/></a>
+            </div>
+            <div className={classes.git}>
+                <a href="https://github.com/AakashVarma1995/justclip.me" target="_blank" rel="noreferrer"><img src={'/assets/github.svg'} style={{height:'32px'}} alt="GitHub Repo Link"/></a>
             </div>
         </div>
     )
